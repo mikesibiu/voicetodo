ab5f132068bb22af5e5a3a3668f5c1b2b61a6150
mikef <mfarace@backblaze.com>
2026-04-12 00:52:14 +0300

    Rename app from VoiceList to VoiceTodo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'voicelist-v1';
+const CACHE = 'voicetodo-v1';
 const PRECACHE = ['/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
